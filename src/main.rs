6162442893b897933793103f0e0033dfeedb2dcc
Mike-Wazowsk1 <exectai@gmail.com>
2023-07-27 22:19:10 +0300

    fix mul

--- src/main.rs
+++ src/main.rs
@@ -502,6 +502,7 @@ async fn get_nft_by_address(address: web::Path<String>) -> impl Responder {
     let client: GasOracleMiddleware<Provider<Http>, GasNow> =
         GasOracleMiddleware::new(provider, gas_oracle);
     let mut nfts: Vec<TokenLocal> = make_nft_array(connection).await;
+    println!("NFTS: {:?}",nfts);
     let tmp_a = &address.clone();
     if tmp_a == "0x289140cbe1cb0b17c7e0d83f64a1852f67215845" {
         let mut res: Vec<TokenLocalTmp> = Vec::new();
