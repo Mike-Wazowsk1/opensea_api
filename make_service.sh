4e3dd2f869cca301319a9850d7826b8de355de8c
Mike-Wazowsk1 <exectai@gmail.com>
2023-09-28 18:13:36 +0300

    service files

---
+++ make_service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "[Unit]
+Description=Bitgesell lotto service
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=$PWD
+ExecStart=$PWD/target/release/opensea_api
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/lotto.service
+echo "Service build and moved"
+systemctl enable lotto.service
+echo "Service enabled for start at boot"
+systemctl start lotto.service
+echo "Service started"
+
+
+
